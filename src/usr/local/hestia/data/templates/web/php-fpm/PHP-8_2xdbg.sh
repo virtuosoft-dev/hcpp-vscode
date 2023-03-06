11fdf9cb47af2fa930ec61cf1466d9f7f13939f2
Stephen J Carnam <steveorevo@gmail.com>
2023-03-06 11:30:10 -0800

    wip

--- src/usr/local/hestia/data/templates/web/php-fpm/PHP-8_2xdbg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/local/hestia/bin/v-add-user-notification admin "VSCode" "&#128309; PHP-8_2xdbg.sh ran."
--- vscode.php
+++ vscode.php
@@ -12,11 +12,29 @@
 if ( ! class_exists( 'VSCode') ) {
     class VSCode {
         /**
-         * Constructor,...
+         * Constructor, register our actions.
          */
         public function __construct() {
             global $hcpp;
             $hcpp->vscode = $this;
+            $hcpp->add_action( 'priv_change_web_domain_backend_tpl', [ $this, 'priv_change_web_domain_backend_tpl' ] );
+        }
+
+        // Allocate port for VSCode Server when using a PHP-FPM xdbg template
+        public function priv_change_web_domain_backend_tpl( $data ) {
+            global $hcpp;
+            $user = $data[0];
+            $domain = $data[1];
+            $tpl = $data[2];
+            if ( strpos( $tpl, 'xdbg' ) === false ) return $data;
+                
+            
+            // 19:16:51.99 [
+            //     "homestead",
+            //     "test1.openmy.info",
+            //     "PHP-8_2xdbg"
+            // ]
+            return $data;
         }
     }
     new VSCode();
