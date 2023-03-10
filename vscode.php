793543cccf6f42918fadc68fe763edd628dac384
Stephen J Carnam <steveorevo@gmail.com>
2023-03-09 19:12:34 -0800

    wip

--- vscode.php
+++ vscode.php
@@ -61,6 +61,7 @@ if ( ! class_exists( 'VSCode') ) {
             global $hcpp;
             $hcpp->log( 'vscode->setup(' . $user . ')' );
             $hostname = trim( $hcpp->delLeftMost( shell_exec( 'hostname -f' ), '.' ) );
+            $pm2 = trim( shell_exec( 'which pm2' ) );
             $conf = "/home/$user/conf/web/vscode-$user.$hostname/nginx.conf";
 
             // Create the configuration folder
@@ -107,10 +108,10 @@ if ( ! class_exists( 'VSCode') ) {
             }
 
             // Start the VSCode Server instance
-            $cmd = "pm2=$(which pm2);runuser -l $user -c \"cd /opt/vscode;\$pm2 pid vscode-$user.$hostname\"";
+            $cmd = "pm2=$(which pm2);runuser -l $user -c \"cd /opt/vscode;$pm2 pid vscode-$user.$hostname\"";
             $hcpp->log( $cmd );
             if ( trim( shell_exec( $cmd ) ) === '' ) {
-                $cmd = "pm2=$(which pm2);runuser -l $user -c \"cd /opt/vscode;\$pm2 start vscode.config.js\"";
+                $cmd = "pm2=$(which pm2);runuser -l $user -c \"cd /opt/vscode;$pm2 start vscode.config.js\"";
                 $hcpp->log( $cmd );
                 $hcpp->log( shell_exec( $cmd ) );
             }else{
